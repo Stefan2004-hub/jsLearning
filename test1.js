47e463358336ff1d1ee4f3d6dd798486d816c89c
unknown <d.stef@yahoo.com>
2020-02-03 19:47:20 +0200

    update home 3feb 2020

--- test1.js
+++ test1.js
@@ -1,1 +1,20 @@
-test git
+function Person (props){
+	return (
+		<div className="person">
+		  <h1>{props.name}</h1>
+		  <p>Your age: {props.age}</p>
+		</div>
+	);
+}
+
+// ReactDOM.render(<Person name="Max" age="26"/>, document.querySelector('#p1'));
+// ReactDOM.render(<Person name="Stefan" age="35"/>, document.querySelector('#p2'));
+
+var app = (
+	<div>
+		<Person name = "Max" age = "26"/>
+		<Person name = "Stefan" age = "35"/>
+	</div>
+);
+
+ReactDOM.render(app, document.querySelector("#app"));
