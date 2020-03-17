5af7d2359a1ad9e12a24de315bfd9fe80aaf4ac0
Dumitriu Stefan <stefan.dumitriu@hartehanks.com>
2020-03-17 18:05:21 +0200

    from home 17mar 2020

---
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger';
+import BuildControls from '../../components/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+    // constructor (props) {
+    //     super(props);
+    //     this.state = {...};
+    // }
+    state = {
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
+    };
+    render() {
+        return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
+            </Aux>
+        )
+
+    }
+}
+
+export default BurgerBuilder;
